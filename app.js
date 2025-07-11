feb1c862f58874ff7e96108cc1b9f3f3571c19d2
WillJCabe <will.j.cabe@gmail.com>
2025-07-10 21:02:30 -1000

    implementation of the User Interface

--- app.js
+++ app.js
@@ -1,57 +1,76 @@
 let humanScore = 0;
 let computerScore = 0;
 
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const container = document.querySelector(".btn-ctn");
+const resultContainer = document.querySelector(".result-ctn");
+
+container.addEventListener("click", (e) => {
+    while (resultContainer.firstChild) {
+  resultContainer.removeChild(resultContainer.firstChild);
+}
+    playRound(e.target.id, getComputerChoice()); 
+})
+
+function showResult(player, computer) {
+    let p = document.createElement("img");
+    p.src = `./assets/${player}.jpg`;
+    p.classList.add("choice-img");
+    let c = document.createElement("img");
+    c.src = `./assets/${computer}.jpg`;
+    c.classList.add("choice-img");
+    resultContainer.appendChild(p);
+    resultContainer.appendChild(c);
+}
+
+
 function getComputerChoice() {
     let choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(Math.random() * 3)];
 }
 
-function getHumanChoice() {
-    let choices = ["rock", "paper", "scissors"];
-    let user = parseInt(prompt("Please type 1 for rock, 2 for paper, or 3 for scissors"));
-    return choices[user - 1];
-}
-
-function playRound(humanChoice, computerChoice) {
+function playRound(humanChoice, computerChoice) {    
     if (humanChoice === computerChoice)  {
-        alert(`You both picked ${humanChoice}, it's a tie.`)
+        showResult(humanChoice, computerChoice);
     } else if (humanChoice === "rock") {
         if (computerChoice === "scissors") {
-            alert(`You won, ${humanChoice} beats ${computerChoice}`);
+            showResult(humanChoice, computerChoice);
             humanScore++;
         } else {
-            alert(`You lost, ${computerChoice} beats ${humanChoice}`);
+            showResult(humanChoice, computerChoice);
             computerScore++;
         }
     } else if (humanChoice === "paper") {
         if (computerChoice === "rock") {
-            alert(`You won, ${humanChoice} beats ${computerChoice}`);
+            showResult(humanChoice, computerChoice);
             humanScore++;
         } else {
-            alert(`You lost, ${computerChoice} beats ${humanChoice}`);
+            showResult(humanChoice, computerChoice);
             computerScore++;
         }
     } else if (humanChoice === "scissors") {
         if (computerChoice === "paper") {
-            alert(`You won, ${humanChoice} beats ${computerChoice}`);
+            showResult(humanChoice, computerChoice);
             humanScore++;
         } else {
-            alert(`You lost, ${computerChoice} beats ${humanChoice}`);
+            showResult(humanChoice, computerChoice);
             computerScore++;
         }
     }
 
 }
 
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
-    if (humanScore == computerScore) alert(`You tied with both ${humanScore} points.`);
-    else if (humanScore > computerScore) alert(`You won with ${humanScore} points against ${computerScore} points.`);
-    else if (humanScore < computerScore) alert(`You lost with ${humanScore} points against ${computerScore} points.`)
-}
+// function playGame() {
+//     for (let i = 0; i < 5; i++) {
+//         let humanSelection = getHumanChoice();
+//         let computerSelection = getComputerChoice();
+//         playRound(humanSelection, computerSelection);
+//     }
+//     if (humanScore == computerScore) alert(`You tied with both ${humanScore} points.`);
+//     else if (humanScore > computerScore) alert(`You won with ${humanScore} points against ${computerScore} points.`);
+//     else if (humanScore < computerScore) alert(`You lost with ${humanScore} points against ${computerScore} points.`)
+// }
 
-playGame();
+// playGame();
