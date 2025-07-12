bd5f020217e28ee7c16a4520442d713968121591
WillJCabe <will.j.cabe@gmail.com>
2025-07-11 14:39:25 -1000

    finish the raw ui and game mechanic

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
 const container = document.querySelector(".btn-ctn");
 const resultContainer = document.querySelector(".result-ctn");
+const score = document.querySelector(".score");
+const resultText = document.querySelector(".result-txt");
 
 container.addEventListener("click", (e) => {
     while (resultContainer.firstChild) {
@@ -32,43 +34,58 @@ function getComputerChoice() {
 function playRound(humanChoice, computerChoice) {    
     if (humanChoice === computerChoice)  {
         showResult([humanChoice, computerChoice]);
+         resultText.innerText = "You tied this round"
     } else if (humanChoice === "rock") {
         if (computerChoice === "scissors") {
             showResult([humanChoice, computerChoice]);
             humanScore++;
+            resultText.innerText = "You won this round"
         } else {
             showResult([humanChoice, computerChoice]);
             computerScore++;
+             resultText.innerText = "You lost this round"
         }
     } else if (humanChoice === "paper") {
         if (computerChoice === "rock") {
             showResult([humanChoice, computerChoice]);
             humanScore++;
+             resultText.innerText = "You won this round"
         } else {
             showResult([humanChoice, computerChoice]);
             computerScore++;
+            resultText.innerText = "You lost this round"
         }
     } else if (humanChoice === "scissors") {
         if (computerChoice === "paper") {
             showResult([humanChoice, computerChoice]);
             humanScore++;
+            resultText.innerText = "You won this round"
         } else {
             showResult([humanChoice, computerChoice]);
             computerScore++;
+            resultText.innerText = "You lost this round"
         }
     }
-
+    score.innerHTML = `Score: ${humanScore} / ${computerScore}`;
+    endGame();
 }
 
-// function playGame() {
-//     for (let i = 0; i < 5; i++) {
-//         let humanSelection = getHumanChoice();
-//         let computerSelection = getComputerChoice();
-//         playRound(humanSelection, computerSelection);
-//     }
-//     if (humanScore == computerScore) alert(`You tied with both ${humanScore} points.`);
-//     else if (humanScore > computerScore) alert(`You won with ${humanScore} points against ${computerScore} points.`);
-//     else if (humanScore < computerScore) alert(`You lost with ${humanScore} points against ${computerScore} points.`)
-// }
-
-// playGame();
+function endGame() {
+    const end = document.createElement("div");
+    end.classList.add("end");
+    end.addEventListener("click", ()=> {
+        humanScore = 0;
+        computerScore = 0;
+        document.body.removeChild(end);
+        score.innerHTML = `Score: ${humanScore} / ${computerScore}`;
+        resultText.innerText = "";
+    })
+    if (humanScore == 5) {
+        end.innerText = "Congratulations, you won the game."
+        document.body.appendChild(end);
+    }
+    if (computerScore == 5) {
+        end.innerText = "OOPS! Sorry, you lost the game."
+        document.body.appendChild(end);
+    }
+}
